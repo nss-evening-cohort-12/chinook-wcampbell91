dff6eb46964d6f81fb62f1897860af4b3dedca2e
Billy Campbell <wcampbell91@gmail.com>
2021-01-23 11:32:19 -0600

    line item track

--- invoice_37_line_item_count.sql
+++ invoice_37_line_item_count.sql
@@ -1,4 +1,5 @@
-SELECT 
-    invoiceId,
-    COUNT(*)
-    FROM InvoiceLine GROUP BY InvoiceId
+SELECT
+    InvoiceLine.*,
+    Track.Name
+FROM InvoiceLine 
+INNER JOIN Track WHERE InvoiceLine.TrackId = Track.TrackId
